1f11f82ddff68683d4beec59a7efd7045488f1b4
kiwam <kiwam@users.noreply.github.com>
2021-11-16 02:58:29 +0900

    add 07 stage#1

--- 07/VMtranslator.nim
+++ 07/VMtranslator.nim
@@ -1,32 +1,48 @@
-import strutils, os, sugar
+import strutils, os, sugar, strformat
 import CodeWriter, Parser, constVMtranslator
 
-# outfiles will be deleted
-proc genInOutFiles(input: string): (seq[string], seq[string]) =
+# number of outfile must be 1
+proc genInOutFiles(input: string): (seq[string], string) =
   if input.endsWith(".vm"):
-    return (@[input], @[input.replace(".vm", ".asm")])
+    return (@[input], input.replace(".vm", ".asm"))
   let infiles = collect(newSeq):
     for file in walkFiles(input & "/*.vm"):
       file
       # file.replace(input&"/", "")
-  let outfiles = collect(newSeq):
-    for f in infiles:
-      f.replace(".vm", ".asm")
-  return (infiles, outfiles)
+  var outfile = input 
+  if outfile.endsWith('/'):
+    outfile = outfile[0..^2]
+  outfile = outfile & '/' & lastPathPart(outfile)  & ".asm"
+  return (infiles, outfile)
 
+proc writeAsm(parser: Parser, writer: Writer) =
+  let cmdType = parser.cmdType
+  case cmdType:
+  of C_ARITHMETIC:
+    writeArithmetic(writer, parser.cmd)
+  of C_PUSH, C_POP:
+    writePushOrPop(writer, parser.cmdType, parser.arg1, parser.arg2)
+  of C_NOP:
+    # echo "C_NOP"
+    discard
+
+proc translateFiles(infiles: seq[string], outfile: string) =
+  if infiles == @[""]:
+    return
+  var writer: Writer = newCodeWriter(outfile)
+  writer.initStackPointer()
+  for f in infiles:
+    var parser: Parser = newParser(f)
+    while parser.hasMoreCommands():
+      parser.advance()
+      writeAsm(parser, writer)
 
 when isMainModule:
   if paramCount() != 1:
-    echo "Need 1 arg.\nUsage example: ./VMtranslator [file.vm|dirname]"
+    echo fmt"{paramCount()} param(s) counted."
+    echo "usage : ./VMtranslator [file.vm|dirName]"
     quit 1
   
-  var infile: seq[string] = @[]
-  var outfile: seq[string] = @[]
-  let tpl = genInOutFiles(commandLineParams()[0])
-  infile = tpl[0]
-  outfile = tpl[1]
-
-  for f in infile:
-    var p: Parser = newParser(f)
-    while hasMoreCommands(p):
-      advance(p)
+  let (infiles, outfile)= genInOutFiles(commandLineParams()[0])
+  echo outfile
+  translateFiles(infiles, outfile)
--- 07/constVMtranslator.nim
+++ 07/constVMtranslator.nim
@@ -5,10 +5,49 @@ type
     C_ARITHMETIC
     C_PUSH
     C_POP
-    C_LABEL
-    C_GOTO
-    C_IF
-    C_FUNCTION
-    C_RETURN
-    C_CALL
+    # C_LABEL
+    # C_GOTO
+    # C_IF
+    # C_FUNCTION
+    # C_RETURN
+    # C_CALL
     C_NOP
+
+type
+  Registers* = enum
+    R_0 = 0,
+    R_1 = 1,
+    R_2 = 2,
+    R_3 = 3,
+    R_4 = 4,
+    R_5 = 5,
+    R_6 = 6,
+    R_7 = 7,
+    R_8 = 8,
+    R_9 = 9,
+    R_10 = 10,
+    R_11 = 11,
+    R_12 = 12,
+    R_13 = 13,
+    R_14 = 14,
+    R_15 = 15,
+    R_16 = 16
+
+type
+  Regs* = enum
+    R_SP = 0,
+    R_LCL = 1,
+    R_ARG = 2,
+    R_THIS = 3,
+    R_THAT = 4
+
+type
+  Segment* = enum
+    S_ARG = "argument",
+    S_LCL = "local",
+    S_STATIC = "static",
+    S_CONST = "constant",
+    S_THIS = "this",
+    S_THAT = "that",
+    S_POINTER = "pointer",
+    S_TEMP = "temp"
